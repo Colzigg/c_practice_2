829b46bf35703341da9e3cac6c3d05bb74a07c3b
Stas V <stasvoi2004@gmail.com>
2022-11-23 13:54:48 +0300

    task 1 modified

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] StringModification(string[] stringOriginal)
+{
+    int lenArray = 0;
+    for (int i = 0; i < stringOriginal.Length; i++)
+    {
+        if (stringOriginal[i].Length <= 3)
+        {
+            lenArray++;
+        }
+    }
+    string[] stringModified = new string[lenArray];
+    if (lenArray > 0)
+        lenArray = 0;
+    {
+        for (int i = 0; i < stringOriginal.Length; i++)
+        {
+            if (stringOriginal[i].Length <= 3)
+            {
+                stringModified[lenArray] = stringOriginal[i];
+                lenArray++;
+            }
+        }
+    }
+    return stringModified;
+}
+
+void StringOutput(string[] stringArray)
+{
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.WriteLine(stringArray[i]);
+    }
+    Console.WriteLine("---------------");
+}
+
+string[] arrayExampleFirst = new string[] { "hello", "2", "world", ":-)" };
+string[] arrayExampleSecond = new string[] { "1234", "1567", "-2", "computur science" };
+string[] arrayExampleThird = new string[] { "russia", "denmark", "kazan", "3" };
+
+
+//string[] stringModified = StringModification(arrayExampleFirst);
+StringOutput(StringModification(arrayExampleFirst));
+StringOutput(StringModification(arrayExampleSecond));
+StringOutput(StringModification(arrayExampleThird));
